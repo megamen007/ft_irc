b4d217a42885fe2f0ea0100208150c82d6fb5b62
Excalibur007 <130876930+megamen007@users.noreply.github.com>
2024-05-21 05:35:41 +0100

    Add files via upload

---
+++ IRC/IRC_Core/A.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int main ()
+{
+    
+    // creating socket 
+    int client_fd = socket (AF_INET , SOCK_STREAM , 0);
+
+    // specfying the address
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // sending connection request 
+    connect(client_fd, (struct sockaddr* )& server_addr, sizeof(server_addr));
+
+    //sending & receivingn data 
+    const void  *msg = "Hello from The Eternals ...";
+    int s = send(client_fd, msg , sizeof(msg), 0);
+    if (s < 0)
+    {
+        perror("receiving Failed Boss ...");
+        close(client_fd);
+        return 1;
+    }
+    else 
+    {
+        std::cout << s <<  " bytes was sent ..." << std::endl;
+    }
+
+    // char buffer[1024] = {0};
+    // int r = recv(client_fd, buffer, sizeof(buffer), 0);
+    // if (r < 0)
+    // {
+    //     perror("receiving Failed Boss ... ");
+    //     close(client_fd);
+    //     return 1;
+    // }
+    // else if (r == 0)
+    // {
+    //     std::cout << "Connection closed by Icaruis ..." << std::endl;
+    //     close(client_fd);
+    //     return 1; 
+    // }
+    // else 
+    // {
+    //     std::cout << "Received "<< r << "bytes From Celestial ... " << std::endl;
+    //     std::cout << "Doctor DOOM sends to u this Holly Message ..." << buffer << std::endl;
+    // }
+
+    // closing socket 
+    close(client_fd);
+    return 0;
+}
